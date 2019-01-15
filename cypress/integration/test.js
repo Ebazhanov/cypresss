ec8d304d98f411d3efbf977d0cadb088de606b4d
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-01-16 00:43:28 +0100

    init

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,5 +1,11 @@
-describe('My First Cypress test', function () {
-    it('Visits Cypress website and checks the title', function () {
+import {page} from "cypress-page-object";
+
+const loginPage = page({});
+
+context('My Awesome WebSite', () => {
+    it('logs into the website', () => {
+
+
         cy.visit('https://staging.comatch.com/en/registry/consultant', {
             auth: {
                 username: 'comatch',
@@ -7,13 +13,21 @@ describe('My First Cypress test', function () {
             }
         });
 
-        cy.contains('Login here').click();
-        cy.get('[type="email"]').type('ebazhanov@yahoo.com');
-        cy.get('[type="password"]').type('beHappy1');
-        cy.contains('Login').click();
+        loginPage
+            .clickOnLoginHere
+            .fillUsername('ebazhanov@yahoo.com')
+            .fillPassword('beHappy1')
+            .clickOnLoginButton()
+        ;
+
+/*        //cy.contains('Login here').should("be.visible").click();
+        cy.get('').type();
+        cy.get().should("be.visible").type();
+
 
         cy.title().should('eq', 'COMATCH: Login to manage your profile and projects');
         //cy.viewport(768, 1024);
-        cy.screenshot('Screenshot.jpg');
-    })
+        cy.screenshot('Screenshot.jpg');*/
+    });
+
 });
