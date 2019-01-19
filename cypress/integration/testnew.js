a5217115bc7bab947f8f5e7911eae8f19c65fa3d
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-01-19 01:18:47 +0100

    refactoring

--- cypress/integration/testnew.js
+++ cypress/integration/testnew.js
@@ -10,12 +10,10 @@ describe('My First Cypress test', () => {
     });
 
     it('Visits Cypress website and checks the title', () => {
-
-
-        cy.contains('Login here').click();
-        cy.get('[type="email"]').type('ebazhanov@yahoo.com');
-        cy.get('[type="password"]').type('beHappy1');
-        cy.contains('Login').click();
+        cy.contains('Login here').should("be.visible").click();
+        cy.get('[type="email"]').should("be.visible").type('ebazhanov@yahoo.com');
+        cy.get('[type="password"]').should("be.visible").type('beHappy1');
+        cy.contains('Login').should("be.visible").click();
 
         cy.title().should('eq', 'COMATCH: Login to manage your profile and projects');
         //cy.viewport(768, 1024);
@@ -23,12 +21,10 @@ describe('My First Cypress test', () => {
     });
 
     it('Visits Cypress', () => {
-
-
-        cy.contains('Login here').click();
-        cy.get('[type="email"]').type('ebazhanov@yahoo.com');
-        cy.get('[type="password"]').type('beHappy1');
-        cy.contains('Login').click();
+        cy.contains('Login here').should("be.visible").click();
+        cy.get('[type="email"]').should("be.visible").type('ebazhanov@yahoo.com');
+        cy.get('[type="password"]').should("be.visible").type('beHappy1');
+        cy.contains('Login').should("be.visible").click();
 
         cy.title().should('eq', 'COMATCH: Login to manage your profile and projects');
         //cy.viewport(768, 1024);
