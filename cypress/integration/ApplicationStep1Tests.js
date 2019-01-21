685c2e3008843ab3b1331a8e2bc37a66be150700
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-01-21 17:27:17 +0100

    add one more enums + eslint

--- cypress/integration/ApplicationStep1Tests.js
+++ cypress/integration/ApplicationStep1Tests.js
@@ -1,7 +1,7 @@
 import Page from '../page-objects/Page';
 import PersonalInformation from "../page-objects/application-step1/PersonalInformation";
 import ContactInformation from "../page-objects/application-step1/ContactInformation";
-import {titleEnum} from "../page-objects/enums/enums-collection";
+import {errMessContactInfo, errMessPersonalInfo, titleEnum} from "../page-objects/enums/enums-collection";
 
 describe('Application page Step#1 - validation of individual fields', () => {
     beforeEach(() => {
@@ -18,9 +18,9 @@ describe('Application page Step#1 - validation of individual fields', () => {
             .enterFirstName(' ')
             .enterLastName(' ')
             .clickOnSaveAndContinueButton()
-            .assertFirstNameErrorMessage('First name is required')
-            .assertLastNameErrorMessage('Last name is required')
-            .assertBirthYearErrorMessage('Year of birth is required');
+            .assertFirstNameErrorMessage(errMessPersonalInfo.FIRST_NAME)
+            .assertLastNameErrorMessage(errMessPersonalInfo.LAST_NAME)
+            .assertBirthYearErrorMessage(errMessPersonalInfo.BIRTH_YEAR);
     });
 
     it('Contact information block', () => {
@@ -29,9 +29,9 @@ describe('Application page Step#1 - validation of individual fields', () => {
             .enterHouseNumber(' ')
             .enterMobilePhoneNumber('blablabphone')
             .clickOnSaveAndContinueButton()
-            .assertStreetNameErrorMessage('Street is required')
-            .assertHouseNumberErrorMessage('House number is required')
-            .assertMobilePhoneNumberErrorMessage('Mobile phone must be a valid phone number');
+            .assertStreetNameErrorMessage(errMessContactInfo.STREET_NAME)
+            .assertHouseNumberErrorMessage(errMessContactInfo.HOUSE_NUMBER)
+            .assertMobilePhoneNumberErrorMessage(errMessContactInfo.MOBILE_NUMBER);
     });
 
 });
--- cypress/page-objects/enums/enums-collection.js
+++ cypress/page-objects/enums/enums-collection.js
@@ -4,3 +4,15 @@ export const titleEnum = {
         PROFDR: "Prof. Dr.",
         PHD: "PhD"
 };
+
+export const errMessPersonalInfo = {
+        FIRST_NAME: 'First name is required',
+        LAST_NAME: 'Last name is required',
+        BIRTH_YEAR: 'Year of birth is required'
+};
+
+export const errMessContactInfo = {
+        STREET_NAME: 'Street is required',
+        HOUSE_NUMBER: 'House number is required',
+        MOBILE_NUMBER: 'Mobile phone must be a valid phone number'
+};
